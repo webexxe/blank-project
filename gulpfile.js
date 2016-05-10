0d1b876d48565a50f1e54f6d0862f5ced59d0d35
Emre Tekin <web.exxe@gmail.com>
2016-05-10 09:21:04 +0300

    Vendor and Scripts layout created

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
-var gulp = require('gulp'),
+var vendor_js = [
+        //NPM INSTALL
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/owl-carousel-2-beta/dist/owl.carousel.min.js',
+
+        //JS LIB FOLDER
+        'js/lib/smothScroll.min.js'
+    ],
+
+    gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     jsmin = require('gulp-jsmin'),
@@ -6,7 +15,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
 
 
 gulp.task('jade', function () {
@@ -15,6 +25,8 @@ gulp.task('jade', function () {
             pretty: true
         }))
         .pipe(gulp.dest('www/'))
+        .pipe(reload({stream: true}))
+
 });
 
 gulp.task('sass', function () {
@@ -30,22 +42,30 @@ gulp.task('cssprefix', function () {
             browsers: ['last 2 versions'],
             cascade: true
         }))
-        .pipe(concat('style.css'))
-        .pipe(gulp.dest('www/css/'));
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('www/css/'))
+        .pipe(reload({stream: true}))
 });
 
 gulp.task('jsMin', function () {
     gulp.src(['js/**/*.js', '!js/**/_*.js'])
         .pipe(jsmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('www/js'));
+        .pipe(gulp.dest('www/js'))
+        .pipe(reload({stream: true}))
 });
 
 gulp.task('scripts', function () {
-    gulp.src(['www/js/*.js', '!www/js/scripts.js'])
+    gulp.src(['js/*.js', '!js/_*.js'])
         .pipe(uglify())
         .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('www/js'));
+    gulp.src(vendor_js)
+        .pipe(uglify())
+        .pipe(concat('vendor.js'))
         .pipe(gulp.dest('www/js'))
+        .pipe(reload({stream: true}))
+
 });
 
 gulp.task('browser-sync', function () {
@@ -60,8 +80,8 @@ gulp.task('browser-sync', function () {
 gulp.task('watch', function () {
     gulp.watch('jade/**/*.jade', ['jade']);
     gulp.watch('sass/**/*.scss', ['sass', 'cssprefix']);
-    gulp.watch(['js/**/*.js', '!js/**/_*.js'], ['jsMin']);
-    //gulp.watch(['www/js/*.js','!www/js/scripts.js'],['scripts']);
+    //gulp.watch(['js/**/*.js', '!js/**/_*.js'], ['jsMin']);
+    gulp.watch('js/*.js', ['scripts']);
 });
 
-gulp.task('default', ['jade', 'sass', 'cssprefix', 'jsMin', /*'scripts',*/ 'browser-sync', 'watch']);
+gulp.task('default', ['jade', 'sass', 'cssprefix', /*'jsMin',*/ 'scripts', 'browser-sync', 'watch']);
