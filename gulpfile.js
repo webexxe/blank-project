f7dd862781675ef75a6391410c948f41595b541e
Emre Tekin (Netmarble) <emre.tekin@intaktiv.intl>
2017-08-28 11:14:11 +0300

    New font feature

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,116 @@
 //----------------------------------------------------------------------------------------------------------------//
 //GULP REQUIRE
 var
-    gulp = require('gulp'),
-    jade = require('gulp-jade'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload,
+gulp = require('gulp'),
+jade = require('gulp-jade'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat'),
+imagemin = require('gulp-imagemin'),
+pngquant = require('imagemin-pngquant'),
+browserSync = require('browser-sync'),
+reload = browserSync.reload,
 
 //----------------------------------------------------------------------------------------------------------------//
 //VENDOR JS/CSS
-    vendor_js = [
-        //NPM INSTALL
-        'node_modules/jquery/dist/jquery.js',
+vendor_js = [
+    //NPM INSTALL
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/owl.carousel/dist/owl.carousel.js',
 
-        //JS LIB FOLDER
-        '_front/js/lib/smothScroll.min.js'
-    ],
-    vendor_css = [];
+    //JS LIB FOLDER
+    '_front/js/lib/smothScroll.min.js'
+],
+vendor_css = [
+    'node_modules/owl.carousel/dist/assets/owl.carousel.css',
+];
 
 //----------------------------------------------------------------------------------------------------------------//
 //GULP TASKS
 
 gulp
-    .task('jade', function () {
-        gulp.src(['_front/jade/*.jade', '!_front/jade/_*.jade'])
-            .pipe(jade({pretty: true}))
-            .pipe(gulp.dest('www/'))
-            .pipe(reload({stream: true}))
-    })
-    
-    .task('css', function () {
-        gulp.src('_front/sass/**/*.scss')
-            .pipe(sass().on('error', sass.logError))
-            .pipe(sass({outputStyle: 'compressed'}))
-            .pipe(autoprefixer({browsers: ['last 2 versions', '> 5%', 'Firefox ESR']}))
-            .pipe(gulp.dest('www/assets/css'));
-        gulp.src(vendor_css)
-            .pipe(autoprefixer({browsers: ['last 2 versions', '> 5%', 'Firefox ESR']}))
-            .pipe(concat('vendor.css'))
-            .pipe(gulp.dest('www/assets/css/'));
-    })
+.task('jade', function () {
+    gulp.src(['_front/jade/*.jade', '!_front/jade/_*.jade'])
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('www/'))
+        .pipe(reload({
+            stream: true
+        }))
+})
 
-    .task('scripts', function () {
-        gulp.src(['_front/js/**/*.js', '!_front/js/**/_*.js'])
-            .pipe(uglify({mangle: false}))
-            .pipe(concat('scripts.js'))
-            .pipe(gulp.dest('www/assets/js'));
-        gulp.src(vendor_js)
-            .pipe(uglify())
-            .pipe(concat('vendor.js'))
-            .pipe(gulp.dest('www/assets/js'))
-            .pipe(reload({stream: true}))
-    })
+.task('css', function () {
+    gulp.src('_front/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+        }))
+        .pipe(gulp.dest('www/assets/css'));
+    gulp.src(vendor_css)
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+        }))
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('www/assets/css/'));
+})
 
-    .task('imgMin', function () {
-        return gulp.src('_front/img/**/*')
-            .pipe(imagemin({
-                progressive: true,
-                svgoPlugins: [{removeViewBox: false}],
-                use: [pngquant()]
-            }))
-            .pipe(gulp.dest('www/assets/img'));
-    })
+.task('font', function () {
+    gulp.src('_front/scss/fonts/**/*.{eot,svg,ttf,woff,woff2}')
+        .pipe(gulp.dest('www/assets/css/fonts'));
+})
 
-    .task('browser-sync', function () {
-        browserSync({
-            files: 'www/**/*',
-            server: {
-                baseDir: 'www/'
-            },
-            https: false,
-            port: 1900
-        });
-    })
+.task('scripts', function () {
+    gulp.src(['_front/js/**/*.js', '!_front/js/**/_*.js'])
+        .pipe(uglify({
+            mangle: false
+        }))
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('www/assets/js'));
+    gulp.src(vendor_js)
+        .pipe(uglify())
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('www/assets/js'))
+        .pipe(reload({
+            stream: true
+        }))
+})
 
-    //-------------------------------------------------------------------------------------------------------------//
-    //GULP START
-    .task('watch', function () {
-        gulp.watch('_front/jade/**/*.jade', ['jade']);
-        gulp.watch('_front/sass/**/*.scss', ['css']);
-        gulp.watch('_front/js/**/*.js', ['scripts']);
-        gulp.watch('_front/img/**/*', ['imgMin']);
-    })
+.task('imgMin', function () {
+    return gulp.src('_front/img/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('www/assets/img'));
+})
 
-    .task('default', ['jade', 'css', 'scripts', 'imgMin', 'browser-sync', 'watch']);
+.task('browser-sync', function () {
+    browserSync({
+        files: 'www/**/*',
+        server: {
+            baseDir: 'www/'
+        },
+        https: false,
+        port: 1900
+    });
+})
+
+//-------------------------------------------------------------------------------------------------------------//
+//GULP START
+.task('watch', function () {
+    gulp.watch('_front/jade/**/*.jade', ['jade']);
+    gulp.watch('_front/scss/**/*.scss', ['css']);
+    gulp.watch('_front/scss/fonts/**/*.{eot,svg,ttf,woff,woff2}', ['font']);
+    gulp.watch('_front/js/**/*.js', ['scripts']);
+    gulp.watch('_front/img/**/*', ['imgMin']);
+})
+
+.task('default', ['jade', 'css', 'font', 'scripts', 'imgMin', 'browser-sync', 'watch']);
